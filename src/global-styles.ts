61ddd1a03f8694d4c8416ac0c3518b9670f0a6bf
ms <archibald.ff14@gmail.com>
2022-06-05 09:54:47 +0900

    style.css add

---
+++ src/global-styles.ts
@@ -0,0 +1,77 @@
+import { createGlobalStyle } from "styled-components";
+
+import { pxToRem } from "./utils/pxToRem";
+// import { Color } from "csstype";
+
+export const GlobalStyles = createGlobalStyle`
+  /* Box sizing rules */
+  *,
+  *::before,
+  *::after {
+  box-sizing: border-box;
+  }
+
+  /* Remove default margin */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd,
+  input {
+      margin: 0;
+  }
+
+  input {
+      padding: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4 {
+      font-weight: normal;
+  }
+
+  html {
+      font-size: 62.5%;
+      text-size-adjust: 100%;
+  }
+
+  body {
+      min-height: 100vh;
+      text-rendering: optimizeSpeed;
+      line-height: 1.5;
+      font-size: ${pxToRem(16)};
+  }
+
+  a {
+      text-decoration-skip-ink: auto;
+      text-decoration: none;
+  }
+
+  img,
+  picture {
+      max-width: 100%;
+      display: block;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+      font: inherit;
+      outline: none;
+      border: none;
+      appearance: none;
+  }
+
+  button {
+      border: 0;
+      background: transparent;
+  }
+  `;
